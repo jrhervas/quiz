e11da412399bcc1e5b1146171c221332fad16ea8
jr <jr@localhost.localdomain>
2015-08-03 14:36:34 +0200

    Indice tematico (tema) para cada pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -39,7 +39,7 @@ exports.answer = function (req, res){
 // GET /quizes/new
 exports.new = function (req, res){
     var quiz = models.Quiz.build(
-        {pregunta: "Pregunta", respuesta: "Respuesta"}
+        {pregunta: "Pregunta", respuesta: "Respuesta", tema: "Tema"}
     );
     res.render('quizes/new', {quiz: quiz, errors:[]});
 };
@@ -55,7 +55,7 @@ exports.create = function(req, res){
         }
         else{
             //guarda en DB los campos pregunta y respuesta de quiz
-            quiz.save({fields: ["pregunta", "respuesta"]})
+            quiz.save({fields: ["pregunta", "respuesta", "tema"]})
             .then(function(){ res.redirect('/quizes')}) //Redireccion HTTP (URL relativo) lista de preguntas
         }
     });
@@ -72,6 +72,7 @@ exports.edit = function(req, res){
 exports.update = function(req, res){
     req.quiz.pregunta  = req.body.quiz.pregunta;
     req.quiz.respuesta = req.body.quiz.respuesta;
+    req.quiz.tema = req.body.quiz.tema;
 
     req.quiz
     .validate()
@@ -82,7 +83,7 @@ exports.update = function(req, res){
             }
             else{
                 req.quiz
-                .save( {fields: ["pregunta", "respuesta"]})
+                .save( {fields: ["pregunta", "respuesta", "tema"]})
                 .then( function(){ res.redirect('/quizes');});
             }
         }
