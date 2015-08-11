21a5dc5ec216125907b3956fdf542993748e3855
jr <jr@localhost.localdomain>
2015-08-11 13:32:27 +0200

    Moderacion de comentarios

--- models/comment.js
+++ models/comment.js
@@ -1,11 +1,15 @@
 // Definicion del modelo de Comment con validacion
-
 module.exports = function(sequelize, DataTypes){
   return sequelize.define(
     'Comment',
-    { texto: {
+    {
+      texto: {
       type: DataTypes.STRING,
       validate: { notEmpty: {msg: "-> Falta Comentario"}}
+      },
+      publicado: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
       }
     }
   );
--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,8 @@ router.get('/', function(req, res) {
 router.get('/author', staticsController.author);
 
 // Autoload de comandos con :quizId
-router.param('quizId', quizController.load);    // autoload :quizId
+router.param('quizId', quizController.load);      // autoload :quizId
+router.param('commentId', commentController.load);// autoload :commentId
 
 // Definicion de rutas de sesion
 router.get('/login', sessionController.new);      // formulario de login
@@ -32,7 +33,9 @@ router.get('/quizes/:quizId(\\d+)/edit',sessionController.loginRequired, quizCon
 router.put('/quizes/:quizId(\\d+)',sessionController.loginRequired, quizController.update);
 router.delete('/quizes/:quizId(\\d+)',sessionController.loginRequired, quizController.destroy);
 
+// Definicion de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
